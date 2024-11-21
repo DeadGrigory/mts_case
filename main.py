5665006bd60ccaa4ccf36311d7b5bb86d232a195
roman <roman.bautin@gmail.com>
2024-11-21 23:10:06 +0300

    ok

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/quiz")
+def quiz():
+    return render_template("quiz.html")
+
+@app.route("/cards")
+def cards():
+    return render_template("cards.html")
+
+if __name__ == "__main__":
+    app.run(port=8080, host="127.0.0.1")
