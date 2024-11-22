e9194ea6fa92ce5e5bc1fef28dcc93806db69a43
roman <roman.bautin@gmail.com>
2024-11-22 10:45:42 +0300

    ok

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
-
 @app.route("/")
 @app.route("/home")
 def home():
@@ -14,13 +13,16 @@ def home():
 def quiz():
     return render_template("quiz.html")
 
+
 @app.route("/games")
 def menu():
     return render_template("games.html")
 
+
 @app.route("/cards")
 def cards():
     return render_template("cards.html")
+
 
 if __name__ == "__main__":
     app.run(port=8080, host="127.0.0.1")
